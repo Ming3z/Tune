e49e62cc41c7fd6eb5d3d6ba13f3e1965e11fd69
YMK_PC <1713442739@qq.com>
2025-11-19 00:10:22 +0800

    初始化项目

---
+++ app/src/main/java/com/ymk/tune/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ymk.tune;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ymk.base.base.BaseActivity;
+import com.ymk.tune.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    @Override
+    protected int initContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
